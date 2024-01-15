acc53da5a31fe475d2b20d8ece5f06fac207fe59
Sompon Onlam <101684583+236sec@users.noreply.github.com>
2024-01-15 21:08:19 +0700

    Create 207_Course_Schedule.cpp

---
+++ leetcode/medium/207_Course_Schedule.cpp
@@ -0,0 +1,71 @@
+#define MAX_N 2010
+vector<list<int>> adj(MAX_N);
+vector<int> indeg(MAX_N);
+vector<bool> visited(MAX_N);
+
+void print_graph(int numCourses) {
+    for (int i = 0; i < numCourses; i++) {
+        if (!adj[i].empty()) {
+            cout << "Graph " << i << " : ";
+            for (auto j = adj[i].begin(); j != adj[i].end(); j++) {
+                cout << *j << " ";
+            }
+            cout << endl;
+        }
+    }
+}
+
+bool iscycle(list<int> &que,int numCourses){
+    int count = 0;
+    while(!que.empty()){
+        int nnode = que.size();
+        count+=nnode;
+        for(int i = 0 ; i < nnode ; i++){
+            int pos = que.front();
+            que.pop_front();
+            for(auto j = adj[pos].begin() ; j != adj[pos].end() ; j++){
+                indeg[*j]-=1;
+                if(indeg[*j] == 0){
+                    visited[*j] = 1;
+                    que.push_back(*j);
+                }
+            }
+        }
+    }
+    if(count == numCourses){
+        return false;
+    }
+    return true;
+}
+
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        for (auto& prereq : prerequisites) {
+            int course = prereq[1];
+            int prerequisite = prereq[0];
+            //cout << prerequisite << " " << course << endl;
+            adj[prerequisite].push_back(course);
+            indeg[course]+=1;
+        }
+        list<int> que;
+        for (int i = 0; i < numCourses; i++) {
+            if(indeg[i] == 0){
+                que.push_back(i);
+                visited[i] = 1;
+            }
+        }
+        bool result = !iscycle(que,numCourses);
+        for (int i = 0; i < numCourses; i++) {
+            adj[i].clear();
+            visited[i] = 0;
+            indeg[i] = 0;
+        }
+        que.clear();
+        if(result){
+            return true;
+        }else{
+            return false;
+        }
+    }
+};
