4e91758bd4f28524ca37049805ff66614c73e35a
236sec <popole159za@gmail.com>
2024-11-05 16:21:09 +0700

    Add Problem 20 Valid Parentheses

---
+++ leetcode/easy/20_Valid_Parentheses.ts
@@ -0,0 +1,24 @@
+function isValid(s: string): boolean {
+  const myhash: { [key: string]: string } = { "(": ")", "{": "}", "[": "]" };
+  const mystack: string[] = [];
+  for (const char of s) {
+    if (char in myhash) {
+      mystack.push(char);
+    } else {
+      if (mystack.length === 0) {
+        return false;
+      }
+
+      const tmp = mystack.pop() as string;
+      if (char !== myhash[tmp]) {
+        return false;
+      }
+    }
+  }
+  if (mystack.length !== 0) {
+    return false;
+  }
+  return true;
+}
+
+console.log(isValid("()"));
