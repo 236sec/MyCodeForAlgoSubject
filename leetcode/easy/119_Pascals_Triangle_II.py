c700501d75ff6a90fdf4005dc65a2080026f2657
236sec <popole159za@gmail.com>
2026-06-29 00:51:34 +0700

    Fix Pascals Triangle ||

--- leetcode/easy/119_Pascals_Triangle_II.py
+++ leetcode/easy/119_Pascals_Triangle_II.py
@@ -1,7 +1,7 @@
 class Solution:
     def getRow(self, rowIndex: int) -> List[int]:
         ans = [1]
-        for i in range(1,rowIndex+1):
-            val = int(ans[-1] * ((rowIndex-i+1)/i))
+        for i in range(1, rowIndex + 1):
+            val = ans[-1] * (rowIndex - i + 1) // i
             ans.append(val)
         return ans
